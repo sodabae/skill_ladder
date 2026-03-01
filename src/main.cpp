fad83d976411161482f2e35f68b7fdf0bc25f802
S <sodaworkbae@gmail.com>
2026-03-01 15:59:36 -0500

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -7,38 +7,9 @@
 #include "updateEvent.h"
 #include "MovementSystem.h"
 
-struct objectMoved
-{
-    float x;
-    float y;
-};
-
-struct damageTaken
-{
-    int amount;
-};
-
 int main()
 {
     eventBus bus;
-
-    auto oId = bus.subscribe<objectMoved>([](const objectMoved& e)
-        {
-            std::cout << "Object moved to "
-                      << e.x << ", " << e.y << "\n";
-        });
-
-    auto dId = bus.subscribe<damageTaken>([](const damageTaken& e)
-        {
-            std::cout << "Damage: "
-                      << e.amount << "\n";
-        });
-
-    objectMoved ev{10.f, 20.f};
-    //volatile auto* debugAddr = &ev;
-    damageTaken dmg{5};
-    bus.publish(ev);
-    bus.publish(dmg);
 
     MovementSystem movement(bus);
 
@@ -51,6 +22,4 @@ int main()
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 
-    //bus.unsubscribe<objectMoved>(oId);
-    //bus.unsubscribe<damageTaken>(dId);
 }
