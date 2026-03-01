1b4a99cb11a834030347828d00c153e12bfcaa9a
S <sodaworkbae@gmail.com>
2026-02-28 19:08:03 -0500

    added unsubscriber added compile options (warnings) to CMake file

--- src/event/eventBus.h
+++ src/event/eventBus.h
@@ -4,8 +4,15 @@
 #include <unordered_map>
 #include <vector>
 #include <typeindex>
+#include <algorithm>
 
 #include "event.h" 
+
+struct handlerCB
+{
+    std::size_t id;
+    std::function<void(const void*)> fn;
+};
 
 class eventBus
 {
@@ -13,14 +20,36 @@ public:
     template<typename EventT>
     using Handler = std::function<void(const EventT&)>;
 
+    
     template<typename EventT>
-    void subscribe(Handler<EventT> handler)
+    [[nodiscard]]size_t subscribe(Handler<EventT> handler)
     {
         auto wrapper = [handler](const void* eventPtr)
         {
             handler(*static_cast<const EventT*>(eventPtr));
         };
-        m_subscribers[typeid(EventT)].push_back(wrapper);
+        m_subscribers[typeid(EventT)].push_back(handlerCB{m_nextId, wrapper});
+        return m_nextId++;
+
+    }
+
+    template<typename EventT>
+    void unsubscribe(std::size_t id)
+    {
+        //get the map iterators of the std::vector from the map
+        auto it = m_subscribers.find(typeid(EventT));
+        if (it != m_subscribers.end())
+        {
+            auto& vec = it->second;
+
+            //erase from the iterator to the end of the vector
+            //  Note: the remove_if returns an iterator and puts the matching element at the end
+            vec.erase(
+                //remove_if - puts the item to be erased at the end
+                std::remove_if(vec.begin(), vec.end(), [id](const handlerCB &item){return item.id==id;}),
+                vec.end() 
+            );
+        }
     }
 
     template<typename EventT>
@@ -34,7 +63,7 @@ public:
         {        
             for (auto& handler : it->second)
             {
-                handler(&event);
+                handler.fn(&event);
             }
         }
     }
@@ -43,5 +72,7 @@ private:
     using RawHandler = std::function<void(const void*)>;
 
     std::unordered_map<std::type_index,
-                       std::vector<RawHandler>> m_subscribers;
+                       std::vector<handlerCB>> m_subscribers;
+    
+    std::size_t m_nextId{0};
 };
--- src/main.cpp
+++ src/main.cpp
@@ -16,21 +16,24 @@ int main()
 {
     eventBus bus;
 
-    bus.subscribe<objectMoved>([](const objectMoved& e)
+    auto oId = bus.subscribe<objectMoved>([](const objectMoved& e)
         {
             std::cout << "Object moved to "
                       << e.x << ", " << e.y << "\n";
         });
 
-    bus.subscribe<damageTaken>([](const damageTaken& e)
+    auto dId = bus.subscribe<damageTaken>([](const damageTaken& e)
         {
             std::cout << "Damage: "
                       << e.amount << "\n";
         });
 
     objectMoved ev{10.f, 20.f};
-    volatile auto* debugAddr = &ev;
+    //volatile auto* debugAddr = &ev;
     damageTaken dmg{5};
     bus.publish(ev);
     bus.publish(dmg);
+
+    bus.unsubscribe<objectMoved>(oId);
+    bus.unsubscribe<damageTaken>(dId);
 }
